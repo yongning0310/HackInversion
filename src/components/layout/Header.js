de0cd5cd50cb4ea9b1e175337debc66e0e225bce
Nicholas Lee <nicholas@nicholas-2.local>
2022-12-18 11:49:10 +0800

    nic

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -112,7 +112,16 @@ const Header = ({
                       navPosition && `header-nav-${navPosition}`
                     )}>
                     <li>
-                      <Link to="#0" onClick={closeMenu}>Documentation</Link>
+                      <Link to="#0" onClick={closeMenu}>Home</Link>
+                    </li>
+                    <li>
+                      <Link to="#0" onClick={closeMenu}>Our Team</Link>
+                    </li>
+                    <li>
+                      <Link to="#0" onClick={closeMenu}>Courses</Link>
+                    </li>
+                    <li>
+                      <Link to="#0" onClick={closeMenu}>Contact Us</Link>
                     </li>
                   </ul>
                   {!hideSignin &&
@@ -120,7 +129,7 @@ const Header = ({
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Enroll Now</Link>
                       </li>
                     </ul>}
                 </div>
